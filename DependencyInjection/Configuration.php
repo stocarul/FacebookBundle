57f30acd919324589240a3a647b35cb87a5fa4ed
Cosmin-Mihai Tutunaru <cosmin@tutunaru.ro>
2014-07-20 19:20:33 +0300

    Add bundle classes.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Stocarul\FacebookBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('stocarul_facebook');
+
+        $rootNode
+            ->children()
+                ->arrayNode('oauth')
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode('app_id')->isRequired()->end()
+                        ->scalarNode('app_secret')->isRequired()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ StocarulFacebookBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stocarul\FacebookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StocarulFacebookBundle extends Bundle
+{
+}
